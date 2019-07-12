c36499e81f0403cae5c264e13d9ec8bc94d1de73
Brandon White <white.brandonsean@gmail.com>
2019-07-11 21:58:34 -0400

    Added Search Bar

--- js/script.js
+++ js/script.js
@@ -4,13 +4,40 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 
+// Creates an element and gives it a property and value
+const createElement = (tag, property, value) => {
+   const element = document.createElement(tag);
+   element[property] = value;
+   return element;
+}
+
+// Appends up to three nested elements
+const appendElements = (parent, child, grandchild1, grandchild2) => {
+   parent.appendChild(child); 
+   child.appendChild(grandchild1);
+      if (grandchild2) {
+         child.appendChild(grandchild2);
+      }
+}
+
 // GLOBAL VARIABLES
 const listItem = document.getElementsByClassName("student-item"); // Grabs list of students from HTML
 const itemsPerPage = 10;
 const pageDiv = document.querySelector(".page");
-const div = document.createElement('div'); // Creates DIV to hold UL
-div.className = 'pagination';              // adds .pagination class to DIV
+const div = createElement('div', 'className', 'pagination'); // Creates DIV w/ class of pagination 
+                                                             // to hold UL
 const ul = document.createElement('ul');   // An UL to hold pagination links
+
+
+// Creates and Appends Elements to add search bar
+const pageHeaderDiv = document.querySelector('.page-header');
+const searchDiv = createElement('div', 'className', "student-search"); // Creates DIV w/ class of sudent-search
+                                                                       // to hold input and button
+const input = createElement('input', 'type', 'text');
+const button = createElement('button', 'textContent', 'Search Students');
+appendElements(pageHeaderDiv, searchDiv, input, button)
+
+
 
 // Function to hide/show list of students
 const showPage = (list, page) => {
@@ -25,11 +52,14 @@ const showPage = (list, page) => {
    } 
 }
 
+
+
 // Function to create pagination links and add functionality to them
 const appendPageLinks = (list) => {
    const pagesNeeded = Math.ceil(list.length/itemsPerPage); // Determines number pagination links needed
-   pageDiv.appendChild(div);
-   div.appendChild(ul);
+   // pageDiv.appendChild(div);
+   // div.appendChild(ul);
+   appendElements(pageDiv, div, ul);
    let i = 1;
    while (i <= pagesNeeded) {
       const li = document.createElement('li');
@@ -38,6 +68,7 @@ const appendPageLinks = (list) => {
       a.href = '#';
       ul.appendChild(li);
       li.appendChild(a);
+      
 
 /*       The following is a condition that makes sure pagination link "1" receives appropriate styling
          on initial load since link would not have been clicked yet | Also calls showPage
@@ -62,3 +93,35 @@ const appendPageLinks = (list) => {
 }
 
 appendPageLinks(listItem);
+
+
+
+
+// pageHeaderDiv.appendChild(searchDiv);
+// searchDiv.appendChild(input);
+// searchDiv.appendChild(button);
+
+/* input.addEventListener('keypress', (e) => {
+   for (let i = 0; i < listItem.length; i++) {
+      listItem[i].style.display = "none";
+   }
+
+
+   let key = e.key.toUpperCase();
+   console.log(key);
+   const details = document.querySelectorAll('.student-details h3');
+   for (let i = 0; i < details.length; i++) {
+      if (details[i].textContent.toUpperCase().startsWith(key)) {
+         listItem[i].style.display = "";
+         const searchArray = [];
+         searchArray.push(listItem[i]);
+         if (searchArray.length <= itemsPerPage) {
+            const lisA = document.querySelectorAll('.pagination ul li a');
+            for (let i = 0; i < lisA.length; i++) {
+               lisA[i].style.display = "none";
+            }
+         } else {            appendPageLinks(searchArray);
+         }
+      }
+   }
+}); */
